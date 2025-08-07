393ce9599a0b0b3804a697aeb6357a9d7cfaa9c2
Ömer Fatih Karakaş <karakasomer2002@email.com>
2025-08-07 16:22:34 +0300

    Navbar sağa yaslama, Current modeli, logout düzeltmesi ve oturum yönetimi iyileştirildi

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,7 +3,7 @@ class MainController < ApplicationController
     if session[:user_id]
       @user = User.find_by(id: session[:user_id])
     end
-    # Ana sayfa için basit bir welcome mesajı
+    
     flash.now[:notice] = "Hoşgeldiniz! Rails uygulamasına başarıyla erişildi."
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,22 @@
 class SessionsController < ApplicationController
     def new
-     if session[:user_id]
-        @user = User.find_by(id: session[:user_id])
-        redirect_to root_path, notice: "Logged out"
-     end  
+    
+    end
+
+    def create
+        @user = User.find_by(email: params[:email])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to root_path, notice: "Logged in successfully"
+        else
+            flash.now[:alert] = "Invalid email or password"
+            render :new
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        redirect_to root_path, notice: "Logged out successfully"
+    end  
+
 end
