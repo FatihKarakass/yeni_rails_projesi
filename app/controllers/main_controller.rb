aca4e4a4fece85f9790758c47043b1a4bbc86e83
Ömer Fatih Karakaş <karakasomer2002@email.com>
2025-08-07 10:06:01 +0300

    MainController düzeltildi: Flash mesaj mantığı iyileştirildi ve flash partial eklendi

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,6 @@
 class MainController < ApplicationController
   def index
+    # Ana sayfa için basit bir welcome mesajı
+    flash.now[:notice] = "Hoşgeldiniz! Rails uygulamasına başarıyla erişildi."
   end
 end
